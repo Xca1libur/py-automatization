8ece649624b34061a7c92a200fdd75a1443b97f2
Mike17 <qqermakov@gmail.com>
2026-09-10 04:45:38 +0400

    improve describe_pet

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ def describe_pet(animal_type, *args):
     if len(args) == 2:
         print(f"His name is {args[0]} and {args[1]} is a {animal_type}")
     elif len(args) == 1:
-        print(f"His name is {args[0]} and {args[0]} is a {animal_type}")
+        print(f"His name is {args[0]} he is a {animal_type}")
     else:
         names = ""
         for name in args:
             if name == args[-1]: 
-                names = names + name
+                names = names + "and " + name
             else:
-                names = names + name + " and "
+                names = names + name + ", "
         print(f"His name are {names}")
 
 describe_pet("cat", "Leva", "Nusha", "Liza", "Dora", "Marsik")
-# describe_pet("cat", "Leva", "Liza")
 # describe_pet("cat", "Leva")
+# describe_pet("cat", "Leva")
+# describe_pet("cat", "Leva", "Nusha", "Liza")
